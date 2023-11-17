2a624bcaf2915e9d60cc50a162383b6678802581
Atulhom <joanguaschduran@gmail.com>
2023-11-17 20:37:57 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pokerNumbers = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+let logos = ["♦", "♥", "♠", "♣"];
+
+document.getElementById("generarBtn").addEventListener("click", function() {
+  let indiceNumeroAleatorio = Math.floor(Math.random() * pokerNumbers.length);
+  let numeroAleatorio = pokerNumbers[indiceNumeroAleatorio];
+
+  let indiceLogosAleatorio = Math.floor(Math.random() * logos.length);
+  let logoAleatorio = logos[indiceLogosAleatorio];
+
+  document.getElementById("resultado-numero").innerHTML = numeroAleatorio;
+  document.getElementById("resultado1-logo").innerHTML = logoAleatorio;
+  document.getElementById("resultado2-logo").innerHTML = logoAleatorio;
+  document.getElementById("resultado2-logo").style.transform = "rotate(180deg)";
+
+  if (logoAleatorio === logos[0] || logoAleatorio == logos[1]) {
+    document.getElementById("resultado1-logo").style.color = "#d00";
+    document.getElementById("resultado2-logo").style.color = "#d00";
+  } else {
+    document.getElementById("resultado1-logo").style.color = "#17202A";
+    document.getElementById("resultado2-logo").style.color = "#17202A";
+  }
+});
